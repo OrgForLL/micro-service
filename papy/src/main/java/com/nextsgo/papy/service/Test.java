fbe324cfb3d7a418cd8f58d9d16578ff133d67ab
halfmoon2014 <mrzhangmh@gmail.com>
2020-03-12 08:04:48 +0800

    test

---
+++ papy/src/main/java/com/nextsgo/papy/service/Test.java
@@ -0,0 +1,18 @@
+package com.nextsgo.papy.service;
+
+import java.util.List;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class Test {
+
+	public void saveZlmxidInfoByZlmxid( String yphh,List<JSONObject> mdata) {
+		
+		for(int i=0;i<mdata.size();i++) {
+			
+		}
+		//String sql="EXEC yf_cx_zlmxidandmlbyzlmxid "+String.valueOf(zlmxid)  ;
+		//NativeQuery<?> nativeQuery = SQLExec();
+		//return nativeQuery.execSql(sql);
+	}
+}
